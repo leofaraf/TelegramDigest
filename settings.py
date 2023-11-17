bf3be92e2e1f6aa019029befad0ccc11a66c293d
Leon Farafonov <leofaraf@gmail.com>
2023-11-17 22:36:50 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,11 +1,11 @@
 # https://my.telegram.org/apps
-API_ID = "24933004"  # api id of telegram account
-API_HASH = "0bac01ab44286f9ba2a19759a484d9b3"  # api hash of telegram account
-PHONE_NUMBER = "79969434611"  # number of telegram account
-DIGEST_CHANNEL = "digestbychatgpt"  # username of digest channel which will use to send message
+API_ID = ""  # api id of telegram account
+API_HASH = ""  # api hash of telegram account
+PHONE_NUMBER = ""  # number of telegram account
+DIGEST_CHANNEL = ""  # username of digest channel which will use to send message
 
 # IMPORTANT: you can use ChatGPT api only which VPN if you're live in Russia
-OPENAI_API_KEY = "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"  # API key for ChatGPT
+OPENAI_API_KEY = ""  # API key for ChatGPT
 OPENAI_SYSTEM_ROLE = ("Ты русскоязычный ассистент саммаризатор, " +
                       "в ответ нужно ввести summary без доп. символов, " +
                       "сообщение должно быть до 200 символов")  # plan of work chat_gpt
